71b92fcc2ef3fd8965e1b6a5d60860f9f617c052
Matt Stine <mstine@gopivotal.com>
2016-01-06 09:54:39 -0600

    initial restaurant code template

---
+++ code/restaurant/src/test/java/com/monolitospizza/RestaurantApplicationTests.java
@@ -0,0 +1,18 @@
+package com.monolitospizza;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RestaurantApplication.class)
+@WebAppConfiguration
+public class RestaurantApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
