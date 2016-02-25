78c216cc2a35569f998ad58dd55e32fc87adbc17
Matt Stine <mstine@gopivotal.com>
2016-02-25 14:31:20 -0600

    add start up mode behavior based on application mode

--- code/restaurant/src/main/java/com/monolitospizza/controllers/HomeController.java
+++ code/restaurant/src/main/java/com/monolitospizza/controllers/HomeController.java
@@ -3,6 +3,7 @@ package com.monolitospizza.controllers;
 import com.monolitospizza.repositories.CustomerRepository;
 import com.monolitospizza.repositories.StoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,22 +17,32 @@ import java.security.Principal;
 @Controller
 public class HomeController {
 
+    private static final String MODE_SITE = "SITE";
+    private static final String MODE_STORE = "STORE";
+
     @Autowired
     CustomerRepository customerRepository;
 
     @Autowired
     StoreRepository storeRepository;
 
+    @Value("${monolitos.mode}")
+    String mode;
+
     @RequestMapping("/")
     public String loadCustomerForHome(Principal principal, ModelMap modelMap, HttpSession httpSession) {
-        Long currentOrderId = (Long) httpSession.getAttribute("currentOrder");
-        if (currentOrderId != null) {
-            return "redirect:/continueOrder";
-        }
+        if (mode.equals(MODE_SITE)) {
+            Long currentOrderId = (Long) httpSession.getAttribute("currentOrder");
+            if (currentOrderId != null) {
+                return "redirect:/continueOrder";
+            }
 
-        String email = principal.getName();
-        modelMap.addAttribute("currentCustomer", customerRepository.findByEmail(email));
-        modelMap.addAttribute("stores", storeRepository.findAll());
-        return "home";
+            String email = principal.getName();
+            modelMap.addAttribute("currentCustomer", customerRepository.findByEmail(email));
+            modelMap.addAttribute("stores", storeRepository.findAll());
+            return "home";
+        } else {
+            return "redirect:/manageOrders";
+        }
     }
 }
